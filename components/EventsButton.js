725792afda78d270255d8e09f50fc93124479639
rjg55 <rikgraham55@gmail.com>
2022-09-12 19:03:04 +0100

    add a 'see events' button beneath map

--- components/EventsButton.js
+++ components/EventsButton.js
@@ -1,15 +1,17 @@
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View } from "react-native";
 
 const EventsButton = ({ navigation }) => {
   return (
-    <View>
-      <Button
-        title="Go to events"
-        onPress={() => navigation.navigate('Events')}>
-        Go to events
-      </Button>
-      <Text>Hello!!!!</Text>
-    </View>
+    <>
+      <View>
+        <Button
+          title="Go to events"
+          onPress={() => navigation.navigate("Events")}
+        >
+          Go to events
+        </Button>
+      </View>
+    </>
   );
 };
 
--- components/Map.js
+++ components/Map.js
@@ -1,18 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
-import EventMarkerList from './EventMarkerList';
-import EventsButton from './EventsButton';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from "expo-status-bar";
+import MapView from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions, Button } from "react-native";
+import EventMarkerList from "./EventMarkerList";
+import EventsButton from "./EventsButton";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Map = ({ navigation }) => {
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, backgroundColor: "#f3f3f3" }}>
       <MapView style={styles.map}>
         <EventMarkerList />
       </MapView>
-
+      <EventsButton
+      // style={{
+      //   position: "absolute",
+      //   // flex: 1,
+      //   height: "100%",
+      //   bottom: 100,
+      //   // zIndex: 10
+      // }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -23,13 +31,19 @@ export default Map;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#F5FCFF",
     alignItems: "center",
     justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-    // height: 500
-  }
+    flex: 1,
+    // zIndex: -1,
+  },
+  actionButton: {
+    position: "absolute",
+    // flex: 1,
+    height: "100%",
+    bottom: 100,
+    // zIndex: 10,
+  },
 });
