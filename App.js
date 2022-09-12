35339c51e18491e5a57464bba4f8a9b653741a17
BumbleGoose <i.r.r.laurie@gmail.com>
2022-09-12 11:45:47 +0100

    add setup for groups components

--- App.js
+++ App.js
@@ -1,30 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import EventsList from './components/EventsList';
-import Navbar from './components/Navbar';
+import { StatusBar } from "expo-status-bar";
+import MapView from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import EventsList from "./components/EventsList";
+import Navbar from "./components/Navbar";
+
+import { UserContext } from "./contexts/UserContext";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Navbar />
-      <MapView style={styles.map}>
-        <EventsList />
-      </MapView>
-      <StatusBar style="auto" />
-    </View>
+    <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
+      <View style={styles.container}>
+        <Navbar />
+        <MapView style={styles.map}>
+          <EventsList />
+        </MapView>
+        <StatusBar style="auto" />
+      </View>
+    </UserContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-  }
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
 });
---
+++ components/GroupListFilter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GroupListFilter = () => {
+  return <div></div>;
+};
+
+export default GroupListFilter;
