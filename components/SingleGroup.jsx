2a389f95c69983bcf54e3b3689cf3b6f97cd8339
BumbleGoose <i.r.r.laurie@gmail.com>
2022-09-13 15:45:12 +0100

    add groups list, single groups with associated group events properly linked

--- components/SingleGroup.jsx
+++ components/SingleGroup.jsx
@@ -1,27 +1,87 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Button } from "react-native";
 import { format } from "date-fns";
-import { getGroupByID } from "../api";
+import { getGroupByID, getEvents } from "../api";
+import { useNavigation } from "@react-navigation/native";
 
 const SingleGroup = ({ route }) => {
+  const navigation = useNavigation();
   const [group, setGroup] = useState({});
   const { _id } = route.params;
-  console.log("this is the id", _id);
+  const [groupEvents, setGroupEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState(null);
 
   useEffect(() => {
-    getGroupByID(_id)
-      .then((fetchedGroup) => {
-        setGroup(fetchedGroup);
+    setIsLoading(true);
+    setErr(null);
+    getGroupByID(_id).then((fetchedGroup) => {
+      setGroup(fetchedGroup);
+      setIsLoading(false);
+    });
+    setIsLoading(true);
+    getEvents()
+      .then((events) => {
+        const filteredGroupEvents = events.filter(
+          (event) => event.group === _id
+        );
+        setGroupEvents(filteredGroupEvents);
+        setIsLoading(false);
       })
-      .catch((err) => console.log("Group err>>>>>>>>", err));
+      .catch((error) =>
+        setErr(`${error.response.status} ${error.response.statusText}`)
+      );
   }, []);
-  console.log(group, "<<<<<<<<<<<group");
+
+  if (isLoading) {
+    return <Text style={styles.description}>Loading...</Text>;
+  }
+
+  if (err) {
+    return <Text style={styles.description}>{err}</Text>;
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{group.title}</Text>
-      <Text style={styles.admin}>Admin: {group.admin}</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>{group.title}</Text>
+        <Text style={styles.details}>Admin: {group.admin}</Text>
+        <Text style={styles.details}>Category: {group.category}</Text>
+        <Text style={styles.details}>
+          Members:
+          {group.members}
+        </Text>
+        <Text style={styles.description}>About: {group.description}</Text>
+      </View>
+      <View>
+        <Text style={styles.details}>Upcoming events:</Text>
+        {groupEvents.map((event) => {
+          const start_time = format(
+            new Date(event.startTime),
+            "d MMM yyyy - h:mm bbb"
+          );
+          const end_time = format(
+            new Date(event.endTime),
+            "d MMM yyyy - h:mm bbb"
+          );
+          return (
+            <View style={styles.container} key={event._id}>
+              <Text style={styles.title}>{event.title}</Text>
+              <Text style={styles.startTime}>Start time: {start_time}</Text>
+              <Text style={styles.endTime}>End time: {end_time}</Text>
+              <Text style={styles.details}>{event.location}</Text>
+              <Text style={""}>{event.attendees}</Text>
+              <Button
+                title="View Event"
+                onPress={() =>
+                  navigation.navigate("Event Details", { _id: event._id })
+                }
+              />
+            </View>
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 };
 
@@ -68,7 +128,7 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingBottom: 5,
   },
-  location: {
+  details: {
     fontSize: 15,
     paddingLeft: 10,
     paddingTop: 10,
